306fd2aab486d1e123acfbc34b97336fa5138e26
Blake Miner <miner.blake@gmail.com>
2026-03-28 02:15:50 -0400

    Updated linter config

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,6 @@
 import js from "@eslint/js"
-import globals from "globals"
 import prettierPlugin from "eslint-plugin-prettier/recommended"
+import globals from "globals"
 
 const nodeBuiltinSet = new Set(Object.keys(globals.nodeBuiltin))
 // Compute the intersection of browser and nodeBuiltin globals
@@ -18,14 +18,22 @@ export default [
 		languageOptions: {
 			ecmaVersion: 2018,
 			sourceType: "module",
-			globals: primaryGlobals,
+			globals: {
+				...primaryGlobals,
+				// AbortController is available in Node 14.17+ and all modern browsers
+				AbortController: "readonly",
+				AbortSignal: "readonly",
+			},
 		},
 		rules: {
 			eqeqeq: ["error", "always", { null: "never" }],
 			"no-unused-expressions": "error",
 			"new-cap": "error",
 			"no-nested-ternary": "error",
-			"no-unused-vars": ["error", { args: "none" }],
+			"no-unused-vars": [
+				"error",
+				{ args: "none", caughtErrorsIgnorePattern: "^ignore" },
+			],
 			"no-var": "error",
 			"no-template-curly-in-string": "error",
 			"no-alert": "error",
@@ -59,6 +67,16 @@ export default [
 	{
 		files: ["example-app/server.js"],
 		languageOptions: {
+			globals: {
+				...globals.node,
+			},
+		},
+	},
+	{
+		// Test files run on Node 18+ and may use any Node global
+		files: ["test/**/*.mjs"],
+		languageOptions: {
+			ecmaVersion: 2022,
 			globals: {
 				...globals.node,
 			},
